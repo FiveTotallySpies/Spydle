cc9cb5599246017a80aef1d6e6ff9954f396a648
klokailo <klo.kai.lo@gmail.com>
2024-10-27 09:07:19 -0600

    Disabled context testing for matchmaker and gameserver

--- gameserver/src/test/java/dev/totallyspies/spydle/gameserver/GameserverApplicationTests.java
+++ gameserver/src/test/java/dev/totallyspies/spydle/gameserver/GameserverApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GameserverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
