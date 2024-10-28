7be935f3325bd0e78e4df390bb1e762563a4bdd2
klokailo <klo.kai.lo@gmail.com>
2024-10-28 21:24:12 +0000

    Created allocation service, enabled GRPC communication with agones allocation

--- matchmaker/src/main/java/dev/totallyspies/spydle/matchmaker/service/GameServerInfo.java
+++ matchmaker/src/main/java/dev/totallyspies/spydle/matchmaker/service/GameServerInfo.java
@@ -1,6 +1,7 @@
 package dev.totallyspies.spydle.matchmaker.service;
 
 import dev.totallyspies.spydle.matchmaker.generated.model.GameServerInfoModel;
+import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
 
@@ -8,6 +9,7 @@ import lombok.ToString;
  * Info we have on each game server that agones spins up
  */
 @Data
+@Builder
 @ToString
 public class GameServerInfo {
 
