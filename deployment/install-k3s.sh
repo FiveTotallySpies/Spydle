9a352247f556374d96c7d864d8071a177bdae98e
klokailo <klo.kai.lo@gmail.com>
2024-11-11 14:38:25 -0500

    Reverted: External node IP requirement, gameserver network policy disabled

--- deployment/install-k3s.sh
+++ deployment/install-k3s.sh
@@ -1,2 +1,2 @@
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable traefik --node-external-ip 172.17.50.3" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable traefik" sh -s -
 sudo chmod 755 /etc/rancher/k3s/k3s.yaml
