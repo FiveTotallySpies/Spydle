08f49a6a0a93fc2d14fa0122840b577fe76665e9
klokailo <klo.kai.lo@gmail.com>
2024-11-24 23:54:29 -0500

    Disabled default game interactors for local profile

--- frontend/src/main/java/dev/totallyspies/spydle/frontend/use_cases/list_games/ListGamesInteractor.java
+++ frontend/src/main/java/dev/totallyspies/spydle/frontend/use_cases/list_games/ListGamesInteractor.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 import java.util.LinkedList;
 
 @Component
-@Profile("!test")
+@Profile("!test & !local")
 public class ListGamesInteractor implements ListGamesInputBoundary {
 
     public final WebClientService webClient;
